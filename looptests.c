f3b497665e180a9e3950583c9b2401847eeac6a6
Nova Tasha <thenovatasha@gmail.com>
2023-08-06 22:44:10 +1000

    Finish 4.01g

--- looptests.c
+++ looptests.c
@@ -5,14 +5,13 @@ int main (int argv, char *argc[])
 {
     int i,j;
     
-    for (i = 1; i < 8; i++)
-     {
-        for (j = i+1; j < 8; j += 3)
-        {
+    for (i=0; i<8; i++) {
+        for (j=i+1; j<8; j+=3) {
+            if (i+j==7) {
+                break;
+            }
             printf("i=%d, j=%d\n", i, j);
         }
-        
-        
     }
     return 0;
 }
