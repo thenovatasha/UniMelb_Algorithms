5b5c387dbc9895cb505d849dca259c458b62cc15
Nova Tasha <thenovatasha@gmail.com>
2023-08-14 01:12:00 +1000

    Tidy delcomm

--- delcomm.c
+++ delcomm.c
@@ -1,78 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argv, char *argc[])
-{
+int main(int argc, char *argv[]) {
     int ch, in_comment = 0, prev_char = ' ';
-   
-    while((ch = getchar()) != EOF)
-    {
+    while((ch = getchar()) != EOF) {
         
-
-        if (in_comment)
-        {
+        if (in_comment) {
             if (ch == '/')
             {
-                if (prev_char == '*')
-                {
+                if (prev_char == '*') {
                     in_comment = 0;
                     prev_char = ' ';
+                    continue;
                 }
-                else
-                {
-                    prev_char = ch;
+            }
+        }
+        else {     
+            if (ch == '*') {
+
+                if (prev_char == '/') {
+                    in_comment = 1;
+                    prev_char = ' ';
+                    continue;
+                }
+                else {
+                    putchar(ch);
+                }
+            }
+            else {   
+                if (ch == '/') {
+
+                    if (prev_char == '/') {
+                        putchar(ch);
+                    }
+                    
+                }
+                else if (prev_char == '/') {
+                    printf("/");
+                    putchar(ch);
+                    
+                }
+                else {
+                    putchar(ch);
                     
                 }
             }
-            else
-            {
-                prev_char = ch;
-                
-            }
         }
-        else
-        {
-            
-            if (ch == '*')
-            {
-                
-                if (prev_char == '/')
-                {
-                    in_comment = 1;
-                    prev_char = ' ';
-                }
-                else
-                {
-                    putchar(ch);
-                    prev_char = ch;
-                }
-            }
-            else
-            {   
-                if (ch == '/')
-                {
-                    if (prev_char == '/')
-                    {
-                        putchar(ch);
-                    }
-                    prev_char = '/';
-                }
-
-                else if (prev_char == '/')
-                {
-                    printf("/");
-                    putchar(ch);
-                    prev_char = ch;
-                }
-                else
-                {
-                    putchar(ch);
-                    prev_char = ch;
-                }
-            }
-        }
-       
+        prev_char = ch;  
     }
-
     return 0;
 }
